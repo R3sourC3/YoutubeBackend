15a80c3b35594de1e07788793d488725586c8d13
Yuvraj Singh Bhadauria <bhadauriasahab23@gmail.com>
2024-02-26 14:33:10 +0530

    controller and routes settings

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@
 
 import dotenv from "dotenv"
 
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
+// import mongoose from "mongoose";
+// import { DB_NAME } from "../constants.js";
 import connectDB from "./db/index.js";
+import {app} from "./app.js"
 
 dotenv.config({
     path:'./env'
@@ -15,7 +16,7 @@ connectDB()
 .then(
     ()=>{
         app.listen(process.env.PORT || 8000,()=>{
-            console.log(`server is running at port ${process.env.PORT}`);
+            console.log(`Server is running at port ${process.env.PORT}`);
         })
     }
 )
