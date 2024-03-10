98baca5bd365f054643f17facd8ad07cf8134e3b
Yuvraj Singh Bhadauria <bhadauriasahab23@gmail.com>
2024-03-10 16:07:46 +0530

    pipelines and sub pipelines

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
+import mongoose from "mongoose";
 
 const GenerateAccessAndRefreshTokens = async(userId) =>{
   try {
@@ -385,6 +386,54 @@ return res.status(200)
 
 })
 
+const getWatchHistory=asyncHandler(async(req,res)=>
+{
+  const user=await User.aggregate([{
+    $match:{
+      _id:new mongoose.Types.ObjectId(req.user._id)
+    }
+  },
+  {
+    $lookup:{
+
+      from:"videos",
+      localField:"watchHistory",
+      foreignField:"_id",
+      as:"watchHsitory",
+      pipeline:[
+        {
+          $lookup:{
+            from:"users",
+            localField:"owner",
+            foreignField:"_id",
+            as:"owner",
+            pipeline:[
+              {
+                $project:{
+                  Fullname:1,
+                  username:1,
+                  avatar:1,
+                }
+              }
+            ]
+          }
+        },
+        {
+          $addFields:{
+            owner:{
+              $first:"$owner"
+            }
+          }
+        }
+      ]
+      
+    }
+  }
+])
+return res.status(200)
+.json(new ApiResponse(200,user[0].watchHistory,"watch history fetched successfuly"))
+})
+
 
 
 
@@ -398,5 +447,6 @@ export {
     updateAccountDetails,
     UpdateUserAvatar,
     UpdateUsercoverImage,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getWatchHistory
 }
